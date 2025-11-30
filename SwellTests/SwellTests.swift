8c152d597ddb3885c6fe4c3cfe440ad6e066be9b
Dhison <dhisonp@gmail.com>
2025-11-30 13:29:37 -0500

    init

---
+++ SwellTests/SwellTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwellTests.swift
+//  SwellTests
+//
+//  Created by Dhison Padma on 11/30/25.
+//
+
+import Testing
+@testable import Swell
+
+struct SwellTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
