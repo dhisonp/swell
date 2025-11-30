151cb2597ed1971f36f5c591438a5cf10e96c5dd
Dhison <dhisonp@gmail.com>
2025-11-30 14:12:19 -0500

    update .gitignore

---
+++ Swell/Models 2/Wave.swift
@@ -0,0 +1,22 @@
+//
+//  Wave.swift
+//  Swell
+//
+//  Created by Dhison Padma on 11/30/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Wave {
+    @Attribute(.unique) var id: UUID
+    var content: String
+    var createdAt: Date
+
+    init(id: UUID = UUID(), content: String, createdAt: Date = Date()) {
+        self.id = id
+        self.content = content
+        self.createdAt = createdAt
+    }
+}
